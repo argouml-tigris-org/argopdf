118de4de05b4fc0373f9863f744a20c1a6b8bba8
Ion Savin <comp_@tigrtis.org>
2007-05-30 18:12:34 +0000

    basic project infrastructure
    git-svn-id: http://argopdf.tigris.org/svn/argopdf/trunk@3 4450cd99-3a2f-0410-a234-dec528f5b2af

---
+++ build.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+# $Id: build.sh 271 2007-02-19 17:58:10Z linus $
+#
+ 
+# +-------------------------------------------------------------------------+
+# | Verify and Set Required Environment Variables                           |
+# +-------------------------------------------------------------------------+
+if [ "$JAVA_HOME" = "" ] ; then
+	echo "***************************************************************"
+	echo "  ERROR: JAVA_HOME environment variable not found."
+	echo ""
+	echo "  Please set JAVA_HOME to the Java JDK installation directory."
+	echo "***************************************************************"
+	exit 1
+fi
+
+#
+# build.sh always calls the version of ant distributed with ArgoUML
+#
+ANT_HOME=../argouml/tools/apache-ant-1.6.5
+
+echo ANT_HOME is: $ANT_HOME
+echo
+echo Starting Ant...
+echo
+
+$ANT_HOME/bin/ant $*
+
+#exit
